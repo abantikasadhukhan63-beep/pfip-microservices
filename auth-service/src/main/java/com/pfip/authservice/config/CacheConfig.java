c9bd414e90c2031fc79bf785ad960c9b39d923bc
Abantika <abantikasadhukhan@gmail.com>
2026-04-23 11:45:58 +0530

    introduced redis cache manager

--- auth-service/src/main/java/com/pfip/authservice/config/CacheConfig.java
+++ auth-service/src/main/java/com/pfip/authservice/config/CacheConfig.java
@@ -6,7 +6,14 @@ import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -23,5 +30,19 @@ public class CacheConfig {
                 .expireAfterWrite(60, TimeUnit.SECONDS)
                 .recordStats());
         return manager;
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory factory){
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer((new GenericJackson2JsonRedisSerializer())));
+
+        return  RedisCacheManager.builder(factory)
+                .cacheDefaults(config)
+                .build();
     }
 }
