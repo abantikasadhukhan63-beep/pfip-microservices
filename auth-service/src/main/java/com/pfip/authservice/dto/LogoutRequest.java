14149a50aa68a3a02bafb10a6685a27342cced27
Abantika <abantikasadhukhan@gmail.com>
2026-04-28 19:50:47 +0530

    Addition of LogoutRequest DTO for handling logout payloads

---
+++ auth-service/src/main/java/com/pfip/authservice/dto/LogoutRequest.java
@@ -0,0 +1,15 @@
+package com.pfip.authservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogoutRequest {
+
+    @NotBlank
+    private String token;
+}
